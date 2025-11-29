fc1412bf01b1a488168ff90c497e463bce81b4d3
Shubha R <shubha180396@gmail.com>
2025-11-29 18:23:41 +0530

    updated app.js

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{id:"heading"},"ReactJS!!!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
 {/* <div id='parent'>
     <div id='child'>
         <h1>Nested H1 Tag</h1>
     </div>
 </div> */}
 
-const parent = React.createElement("div", { id: "parent" },React.createElement("div", { id: "child" },[React.createElement("h1", {},"Nested H1 Tag"),React.createElement("h2", {},"Nested H2 Tag"), React.createElement("h3", {},"Nested H3 Tagdffddf")]));
-
-console.log(parent)// createElement is nothing but the object which has attributes and child
+// const parent = React.createElement("div", { id: "parent" },React.createElement("div", { id: "child" },[React.createElement("h1", {},"Nested H1 Tag"),React.createElement("h2", {},"Nested H2 Tag"), React.createElement("h3", {},"Nested H3 Tagdffddf")]));
+//====================================================================================
+// const heading = React.createElement("h1",{id:"heading"},"ReactJS using core React element");
+//Line 12 and 14 are same
+const jsx = (<h1>React JS using JSX</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-
-root.render(parent);
+root.render(jsx);

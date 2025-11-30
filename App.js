7ba2b21a8022e5b886fb54a66441755b34932854
Shubha R <shubha180396@gmail.com>
2025-11-30 12:16:59 +0530

    added new file appjsref.js

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-{/* <div id='parent'>
-    <div id='child'>
-        <h1>Nested H1 Tag</h1>
-    </div>
-</div> */}
-
-// const parent = React.createElement("div", { id: "parent" },React.createElement("div", { id: "child" },[React.createElement("h1", {},"Nested H1 Tag"),React.createElement("h2", {},"Nested H2 Tag"), React.createElement("h3", {},"Nested H3 Tagdffddf")]));
-//====================================================================================
-// const heading = React.createElement("h1",{id:"heading"},"ReactJS using core React element");
-//Line 12 and 14 are same
+//React Element
 const jsx = (<h1>React JS using JSX</h1>);
+
+//React Component shud start with captial letter
+const JsxComponent =  () =>  (
+   
+        <h1>React JS using JsxComponent</h1>
+    
+);
+//or
+//const JsxComponent =  () => {
+// return <h1>React JS using JSX</h1>
+// };
+
+const JsxComponent1 =  () =>  (
+    <div>
+        <JsxComponent/>
+        <h1>React JS using JsxComponent1</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsx);
+root.render(<JsxComponent1/>);
